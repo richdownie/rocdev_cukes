2972de1a421509e2a7909e24b568d687754d9c52
Rich Downie <rich@blackboxd.com>
2018-04-15 14:08:58 -0400

    Wire up CH

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,5 +1,5 @@
 Before do
-  @cukehub_api_key = ""
+  @cukehub_api_key = "rfviq96hKK7VzuQLnR985q82"
   @domain = "https://rocdev.org"
   puts @browser.capabilities.browser_name + " " + @browser.capabilities.version
   @homepage = HomePage.new(@browser)
